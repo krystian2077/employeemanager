16a937531d8388595cedca2be0460286fbc0a6e7
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-06-11 22:34:09 +0200

    Finish project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
 FILMIK JAK DZIAŁA APLIKACJA
 https://github.com/user-attachments/assets/b773388f-c553-4bd6-8252-e4a72a9d4e65
 
+[employeeManager.mp4](employeeManager.mp4)
--- src/main/java/com/employeemanager/EmployeeResource.java
+++ src/main/java/com/employeemanager/EmployeeResource.java
@@ -4,6 +4,7 @@ import com.employeemanager.model.Employee;
 import com.employeemanager.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeResource {
